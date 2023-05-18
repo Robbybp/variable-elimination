02b071cdce569f1d8747f11cb2468ef3ea940274
robbybp <robbybparker@gmail.com>
2023-05-18 14:15:50 -0600

    make package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="variable-elimination",
+    description=(
+        "Algorithms and test problems for variable elimination in nonlinear"
+        " optimization problems."
+    ),
+    author="Robert Parker and Sakshi Naik",
+    license="BSD-3",
+    packages=["var_elim"],
+)
