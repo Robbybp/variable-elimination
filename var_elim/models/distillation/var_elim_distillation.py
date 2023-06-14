3456ea61db3ebe0b9cb876d2732fe2df650712c6
Sakshi Naik <sakshi21299@gmail.com>
2023-06-14 08:54:47 -0600

    corrected copyright

--- var_elim/models/distillation/var_elim_distillation.py
+++ var_elim/models/distillation/var_elim_distillation.py
@@ -5,7 +5,7 @@
 #  Copyright (c) 2023. Triad National Security, LLC. All rights reserved.
 #
 #  This program was produced under U.S. Government contract 89233218CNA000001
-#  for Los Alamos National Laboratory (LANL), which i"s operated by Triad
+#  for Los Alamos National Laboratory (LANL), which is operated by Triad
 #  National Security, LLC for the U.S. Department of Energy/National Nuclear
 #  Security Administration. All rights in the program are reserved by Triad
 #  National Security, LLC, and the U.S. Department of Energy/National Nuclear
