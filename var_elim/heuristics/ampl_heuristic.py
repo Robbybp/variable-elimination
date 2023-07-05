2f5d556e65c10debcb12e12321d030b4b1103d8b
Sakshi Naik <sakshi21299@gmail.com>
2023-07-05 12:32:44 -0600

    Filter equality constraints before for loop

--- var_elim/heuristics/ampl_heuristic.py
+++ var_elim/heuristics/ampl_heuristic.py
@@ -22,6 +22,7 @@ from pyomo.core.expr.visitor import identify_variables
 from pyomo.repn import generate_standard_repn
 from pyomo.core.base.set import Integers, Binary
 from pyomo.common.collections import ComponentSet
+from pyomo.core.expr.current import EqualityExpression
 import random
 
 
@@ -42,46 +43,47 @@ def identify_vars_for_elim_ampl(m,
     con_list : List of constraints used to eliminate the variables
 
     """
+    #Get active equality constraints from the model
+    cons = []
+    for c in m.component_data_objects(Constraint, active=True):
+        if isinstance(c.expr, EqualityExpression):
+            cons.append(c)
+    
+    #Shuffle constraints for randomizing constraint order
     if randomize == True:
-        #Randomize constraint order
-        cons = list(m.component_data_objects(Constraint, active=True))
         random.shuffle(cons)
-    else:
-        # Get constraint data in the order in which constraints are written
-        cons = list(m.component_data_objects(Constraint, active=True))
-
+   
     # Identify variables of the type ===> coef*v (+/-) expr = 0
     var_list = []
     con_list = []
     defining_var_ids = set()
     for c in cons:
-        if c.equality ==  True:
-            # gets all vars in order from left to right in the constraint expression
-            expr_vars = list(identify_variables(c.expr))
-    
-            # gets linear vars in the constraint expression
-            repn = generate_standard_repn(c.body, compute_values=False, quadratic=False)
-            linear_vars = ComponentSet(repn.linear_vars)
-    
-            nonlinear_vars = ComponentSet(repn.nonlinear_vars)
-    
-            if expr_vars[0].domain is Integers or expr_vars[0].domain is Binary:
-                pass
-            elif not eliminate_bounded_vars and (expr_vars[0].lb is not None or expr_vars[0].ub is not None):
-                pass
-            elif expr_vars[0] in nonlinear_vars:
-                pass
-            elif eliminate_linear_cons_only and len(nonlinear_vars) != 0:
-                pass
-            elif id(expr_vars[0]) not in defining_var_ids:
-                if expr_vars[0] in linear_vars:
-                    var_list.append(expr_vars[0])
-                    con_list.append(c)
-                    for var in expr_vars:
-                        defining_var_ids.add(id(var))
+        # gets all vars in order from left to right in the constraint expression
+        expr_vars = list(identify_variables(c.expr))
 
-                    # This will add all vars from the expression to the defining vars list
-                    # This works because we anyways don't want to eliminate the same var twice
-                    # using 2 different constraints
+        # gets linear vars in the constraint expression
+        repn = generate_standard_repn(c.body, compute_values=False, quadratic=False)
+        linear_vars = ComponentSet(repn.linear_vars)
+
+        nonlinear_vars = ComponentSet(repn.nonlinear_vars)
+
+        if expr_vars[0].domain is Integers or expr_vars[0].domain is Binary:
+            pass
+        elif not eliminate_bounded_vars and (expr_vars[0].lb is not None or expr_vars[0].ub is not None):
+            pass
+        elif expr_vars[0] in nonlinear_vars:
+            pass
+        elif eliminate_linear_cons_only and len(nonlinear_vars) != 0:
+            pass
+        elif id(expr_vars[0]) not in defining_var_ids:
+            if expr_vars[0] in linear_vars:
+                var_list.append(expr_vars[0])
+                con_list.append(c)
+                for var in expr_vars:
+                    defining_var_ids.add(id(var))
+
+                # This will add all vars from the expression to the defining vars list
+                # This works because we anyways don't want to eliminate the same var twice
+                # using 2 different constraints
 
     return var_list, con_list
